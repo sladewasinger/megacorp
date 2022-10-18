f8b538ecb2e08f42a8b721fd834dc348f13b7902
Austin Wasinger <austinwasinger@gmail.com>
2022-10-17 23:00:01 -0600

    Fixed port issue

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import { Server } from "socket.io";
 import express from "express";
 import { createServer } from "http";
 
-const PORT = 3000;
+const port = process.env.PORT || 3000; // Use the port that AWS provides or default to 3000. Without this, the deployment will fail.
+
 const app = express();
 app.use(express.static("app"));
 
 const socketServer = createServer(app);
-socketServer.listen(3000, () => {
-  console.log(`socket io server listening on *:${PORT}`);
+socketServer.listen(port, () => {
+  console.log(`socket io server listening on *:${port}`);
 });
 
 const io = new Server(socketServer);
